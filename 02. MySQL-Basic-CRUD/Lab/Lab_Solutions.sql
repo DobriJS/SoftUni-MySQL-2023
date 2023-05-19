8c2c254afb1842a527f0c2cdb8de9e96dcf06e44
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-19 17:06:09 +0300

    01-03

---
+++ 02. MySQL-Basic-CRUD/Lab/Lab_Solutions.sql
@@ -0,0 +1,21 @@
+-- 01. Select Employee Information
+SELECT id,
+    first_name,
+    last_name,
+    job_title
+FROM employees
+ORDER BY id;
+-- 02. Select Employees with Filter
+SELECT id,
+    CONCAT(`first_name`, ' ', `last_name`) AS 'full_name',
+    job_title,
+    salary
+FROM `employees`
+WHERE salary > 1000
+ORDER BY id;
+-- 03. Update Salary and Select
+UPDATE employees
+SET salary = salary + 100
+WHERE job_title = 'Manager';
+SELECT salary
+FROM employees;
