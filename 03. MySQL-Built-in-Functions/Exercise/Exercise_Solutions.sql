e6705642f88ce42c6f0316e6ddc504d1d0ad6b5f
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-22 11:14:13 +0300

    03

--- 03. MySQL-Built-in-Functions/Exercise/Exercise_Solutions.sql
+++ 03. MySQL-Built-in-Functions/Exercise/Exercise_Solutions.sql
@@ -10,3 +10,9 @@ SELECT first_name,
 FROM employees
 WHERE last_name LIKE '%ei%'
 ORDER BY employee_id;
+-- 3. Find First Names of All Employess
+SELECT first_name
+FROM employees
+WHERE department_id IN (3, 10)
+    AND YEAR(hire_date) BETWEEN 1995 AND 2005
+ORDER BY employee_id ASC;
