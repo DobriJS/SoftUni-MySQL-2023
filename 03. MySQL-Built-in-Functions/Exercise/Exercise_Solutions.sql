68f216acf9f28c0e870f3b11a3e9e07de8d12971
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-22 10:50:08 +0300

    02

--- 03. MySQL-Built-in-Functions/Exercise/Exercise_Solutions.sql
+++ 03. MySQL-Built-in-Functions/Exercise/Exercise_Solutions.sql
@@ -4,3 +4,9 @@ SELECT first_name,
 FROM employees
 WHERE first_name LIKE 'Sa%'
 ORDER BY employee_id;
+-- 2. Find Names of All Employees by Last Name
+SELECT first_name,
+    last_name
+FROM employees
+WHERE last_name LIKE '%ei%'
+ORDER BY employee_id;
