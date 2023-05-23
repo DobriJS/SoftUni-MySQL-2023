1413b306f214010314a0576e51ebbd24a8dfbd51
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-23 12:00:28 +0300

    05, 06

--- 04. MySQL-Data-Aggregation/Exercises/Exercise_Solutions.sql
+++ 04. MySQL-Data-Aggregation/Exercises/Exercise_Solutions.sql
@@ -17,3 +17,16 @@ FROM wizzard_deposits
 GROUP BY deposit_group
 ORDER BY AVG(magic_wand_size)
 LIMIT 1;
+-- 5. Deposits Sum
+SELECT deposit_group,
+    SUM(deposit_amount) AS `total_sum`
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY `total_sum`;
+-- . Deposits Sum for Ollivander Family
+SELECT deposit_group,
+    SUM(deposit_amount) AS `total_sum`
+FROM wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+ORDER BY deposit_group;
