848cb016a1cd8ed43c66868bc9ddc4fb8b6b3d0e
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-23 10:38:26 +0300

    02, 03

--- 04. MySQL-Data-Aggregation/Exercises/Exercise_Solutions.sql
+++ 04. MySQL-Data-Aggregation/Exercises/Exercise_Solutions.sql
@@ -1,3 +1,13 @@
 -- 1. Records' Count
 SELECT COUNT(*) AS 'count'
 from wizzard_deposits;
+-- 2. Longest Magic Wand
+SELECT MAX(magic_wand_size) AS 'longest_magic_wand'
+from wizzard_deposits;
+-- 3. Longest Magic Wand per Deposit Groups
+SELECT deposit_group,
+    MAX(magic_wand_size) AS `longest_magic_wand`
+FROM wizzard_deposits
+GROUP BY deposit_group
+ORDER BY `longest_magic_wand`,
+    deposit_group;
