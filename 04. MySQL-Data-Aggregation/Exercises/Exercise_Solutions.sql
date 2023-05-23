97b45cd073fc9aab59d8d43e631cc86ab2b7bb6c
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-23 12:22:26 +0300

    07, 08

--- 04. MySQL-Data-Aggregation/Exercises/Exercise_Solutions.sql
+++ 04. MySQL-Data-Aggregation/Exercises/Exercise_Solutions.sql
@@ -23,10 +23,27 @@ SELECT deposit_group,
 FROM wizzard_deposits
 GROUP BY deposit_group
 ORDER BY `total_sum`;
--- . Deposits Sum for Ollivander Family
+-- 6. Deposits Sum for Ollivander Family
 SELECT deposit_group,
     SUM(deposit_amount) AS `total_sum`
 FROM wizzard_deposits
 WHERE magic_wand_creator = 'Ollivander family'
 GROUP BY deposit_group
 ORDER BY deposit_group;
+-- 7. Deposits Filter
+SELECT deposit_group,
+    SUM(deposit_amount) AS `total_sum`
+FROM wizzard_deposits
+WHERE magic_wand_creator = 'Ollivander family'
+GROUP BY deposit_group
+HAVING `total_sum` < 150000
+ORDER BY `total_sum` DESC;
+-- 8. Deposit Charge
+SELECT deposit_group,
+    magic_wand_creator,
+    MIN(deposit_charge) AS `min_deposit_charge`
+FROM wizzard_deposits
+GROUP BY deposit_group,
+    magic_wand_creator
+ORDER BY magic_wand_creator,
+    deposit_group;
