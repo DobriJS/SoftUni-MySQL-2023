df91802e6538b0fa23d17c43edae3890071e5632
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-28 12:40:05 +0300

    08

--- 02. MySQL-Basic-CRUD/Exercises/Solutions.sql
+++ 02. MySQL-Basic-CRUD/Exercises/Solutions.sql
@@ -34,3 +34,10 @@ SELECT *
 FROM employees
 WHERE job_title = 'Sales Representative'
 ORDER BY employee_id;
+-- 8. Find Names of All Employees by Salary in Range
+SELECT first_name,
+    last_name,
+    job_title
+FROM employees
+WHERE salary BETWEEN 20000 AND 30000
+ORDER BY employee_id;
