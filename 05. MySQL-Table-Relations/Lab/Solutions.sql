0f34b67e52afb2b23b99caef4a9ddf8d7bfa6261
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-30 23:35:44 +0300

    03

--- 05. MySQL-Table-Relations/Lab/Solutions.sql
+++ 05. MySQL-Table-Relations/Lab/Solutions.sql
@@ -16,6 +16,13 @@ SELECT driver_id,
     CONCAT(first_name, ' ', last_name) AS 'driver_name'
 FROM vehicles
     JOIN campers ON driver_id = campers.id;
+-- 3. SoftUni Hiking
+SELECT starting_point,
+    end_point,
+    campers.id AS 'leader_id',
+    CONCAT(first_name, ' ', last_name) AS 'leader_name'
+FROM routes
+    JOIN campers ON routes.leader_id = campers.id;
 -- 4 Delete Mountains
 CREATE TABLE mountains(
     id INT PRIMARY KEY AUTO_INCREMENT,
