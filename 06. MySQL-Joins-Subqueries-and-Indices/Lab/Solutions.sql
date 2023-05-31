1d55432be512441bd1edf9beeb775f62848d650f
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-05-31 14:01:45 +0300

    02

--- 06. MySQL-Joins-Subqueries-and-Indices/Lab/Solutions.sql
+++ 06. MySQL-Joins-Subqueries-and-Indices/Lab/Solutions.sql
@@ -7,3 +7,12 @@ FROM employees AS e
     RIGHT JOIN departments AS d ON d.manager_id = e.employee_id
 ORDER BY e.employee_id
 LIMIT 5;
+-- 2. Towns and Addresses
+SELECT towns.town_id,
+    `name` AS 'town_name',
+    address_text
+FROM towns
+    JOIN addresses ON towns.town_id = addresses.town_id
+WHERE towns.name IN ('San Francisco', 'Sofia', 'Carnation')
+ORDER BY town_id,
+    address_id;
