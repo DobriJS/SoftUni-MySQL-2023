dd2ecb924511b5eb16df88080ef958bf09e029f2
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-06-02 14:41:25 +0300

    07

--- 06. MySQL-Joins-Subqueries-and-Indices/Exercise/Solutions.sql
+++ 06. MySQL-Joins-Subqueries-and-Indices/Exercise/Solutions.sql
@@ -55,3 +55,15 @@ FROM employees AS e
 WHERE e.hire_date > '1999-01-01'
     AND d.name IN ('Sales', 'Finance')
 ORDER BY e.hire_date;
+-- 7. Employees with Project
+SELECT e.employee_id,
+    e.first_name,
+    p.name AS project_name
+FROM employees AS e
+    JOIN employees_projects AS ep ON e.employee_id = ep.employee_id
+    JOIN projects AS p ON ep.project_id = p.project_id
+WHERE DATE(p.start_date) > '2002-08-13'
+    AND p.end_date IS NULL
+ORDER BY e.first_name,
+    p.name
+LIMIT 5;
