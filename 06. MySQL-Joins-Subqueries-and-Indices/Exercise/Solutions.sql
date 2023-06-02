0109c203bd97d5764b7c26fd547e10ea035d8436
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-06-02 15:22:13 +0300

    10

--- 06. MySQL-Joins-Subqueries-and-Indices/Exercise/Solutions.sql
+++ 06. MySQL-Joins-Subqueries-and-Indices/Exercise/Solutions.sql
@@ -76,3 +76,13 @@ FROM employees AS e
     JOIN projects AS p ON ep.project_id = p.project_id
 WHERE e.employee_id = 24
 ORDER BY p.name;
+-- 10. Employee Summary
+SELECT e.employee_id,
+    CONCAT_WS(' ', e.first_name, e.last_name) AS employee_name,
+    CONCAT_WS(' ', m.first_name, m.last_name) AS manager_name,
+    d.name AS department_name
+FROM employees AS e
+    JOIN employees AS m ON e.manager_id = m.employee_id
+    JOIN departments AS d ON e.department_id = d.department_id
+ORDER BY e.employee_id
+LIMIT 5;
