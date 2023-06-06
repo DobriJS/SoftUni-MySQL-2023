f5ab489b5c85d444b4ed56c1e88ddfe6b8028a94
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-06-06 15:51:21 +0300

    11

--- 07. MySQL-Database-Programmability/Exercise/Solutions.sql
+++ 07. MySQL-Database-Programmability/Exercise/Solutions.sql
@@ -84,6 +84,20 @@ CREATE FUNCTION ufn_calculate_future_value(
     interest_rate_per_year DECIMAL(19, 4),
     number_of_years INT
 ) RETURNS DECIMAL(19, 4) RETURN initial_sum * POW((1 + interest_rate_per_year), number_of_years);
+-- 11. Calculating Interest
+DELIMITER $$ CREATE PROCEDURE usp_calculate_future_value_for_account(
+    account_id INT,
+    interest_rate DECIMAL(19, 4)
+) BEGIN
+SELECT a.id AS 'account_id',
+    h.first_name,
+    h.last_name,
+    a.balance AS 'current_balance',
+    ufn_calculate_future_value(a.balance, interest_rate, 5) AS 'balance_in_5_years'
+FROM `account_holders` AS h
+    JOIN `accounts` AS a ON h.id = a.account_holder_id
+WHERE a.id = account_id;
+END $$ DELIMITER;
 -- 12. Deposit Money
 DELIMITER $$ CREATE PROCEDURE usp_deposit_money(
     account_id INT,
