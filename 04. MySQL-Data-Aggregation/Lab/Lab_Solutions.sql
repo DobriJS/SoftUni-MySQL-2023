519247aa8e3bc7c071a4154122d87434b4641538
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-06-07 14:49:58 +0300

    05

--- 04. MySQL-Data-Aggregation/Lab/Lab_Solutions.sql
+++ 04. MySQL-Data-Aggregation/Lab/Lab_Solutions.sql
@@ -16,3 +16,10 @@ SELECT COUNT(category_id)
 FROM products
 WHERE price > 8
     AND category_id = 2;
+-- 5. Menu Prices
+SELECT category_id,
+    ROUND(AVG(price), 2) AS 'Average Price',
+    ROUND(MIN(price), 2) AS 'Cheapest Product',
+    ROUND(MAX(price), 2) AS 'Most Expensive Product'
+FROM products
+GROUP BY category_id;
