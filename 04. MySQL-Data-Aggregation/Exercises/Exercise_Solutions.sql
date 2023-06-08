ad42864d469c9b02f813c91b1db04f7f5d798ed5
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-06-08 16:11:49 +0300

    17

--- 04. MySQL-Data-Aggregation/Exercises/Exercise_Solutions.sql
+++ 04. MySQL-Data-Aggregation/Exercises/Exercise_Solutions.sql
@@ -125,3 +125,16 @@ SELECT DISTINCT department_id,
 FROM employees
 HAVING third_highest_salary IS NOT NULL
 ORDER BY department_id;
+-- 17. Salary Challenge
+SELECT first_name,
+    last_name,
+    department_id
+FROM employees e1
+WHERE salary > (
+        SELECT AVG(salary)
+        FROM employees e2
+        WHERE e1.department_id = e2.department_id
+    )
+ORDER BY department_id,
+    employee_id
+LIMIT 10;
