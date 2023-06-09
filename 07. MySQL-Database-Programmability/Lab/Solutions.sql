dd237aae5194e83bb67bb6386ba1f61e66b805db
Dobri Dobrevski <dobri.dobrevski@gmail.com>
2023-06-09 14:08:28 +0300

    01

---
+++ 07. MySQL-Database-Programmability/Lab/Solutions.sql
@@ -0,0 +1,9 @@
+-- 1. Count Employees by Town
+CREATE FUNCTION ufn_count_employees_by_town(`town_name` VARCHAR(50)) RETURNS INT DETERMINISTIC BEGIN RETURN (
+    SELECT COUNT(*)
+    FROM employees AS e
+        JOIN addresses AS a ON e.address_id = a.address_id
+        JOIN towns AS t ON a.town_id = t.town_id
+    WHERE t.name = `town_name`
+);
+END
